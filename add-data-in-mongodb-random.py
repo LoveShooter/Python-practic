0d6d3697c283bc9d85bb5b57da7bb27002a24c95
LoveShooter <maxxx.aka.subfocus@gmail.com>
2020-04-11 18:37:58 +0300

    done

--- add-data-in-mongodb-random.py
+++ add-data-in-mongodb-random.py
@@ -3,18 +3,17 @@ import random
 
 client = pymongo.MongoClient('mongodb+srv://sysadm:Ff121314@cluster0-gpxwq.mongodb.net/')  #Connect to my MongoDB cluster + auth
 db = client["random-datadb"]  #Create DB
-mycoll = db["people-orders"]  #Create collection 'people-orders'
+mycoll = db["people-orders-input-name"]  #Create collection 'people-orders-input-name'
 
 mainmenu = ['Beef', 'Potato', 'Tomato', 'Grill', 'Vegetables', 'Chicken', 'Pesto']
 drinks = ['Cola', 'Water', 'Wed wine', 'Vodka', 'Whiskey', 'White wine', 'Orange juice', 'Fanta']
 dessert = ['Cake', 'Cheese cake', 'Jelly', 'Chocolate pie']
-names = ['Max', 'Alex', 'John', 'Alena', 'Arnold', 'Ted', 'Philip', 'Danielle', 'Zane', 'Sirena']
 
 
 for x in range(1, 11):
     orders = {
         'order_number': random.randint(1, 99),
-        'guest_name': random.choice(names),
+        'guest_name': input("Enter your name: "),
         'guest_order': random.choice(mainmenu) + ' , ' + random.choice(drinks) + ' , ' + random.choice(dessert)
 
     }
