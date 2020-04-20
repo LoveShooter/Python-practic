ebc592d99d75208e1198673f6e46ac6d010def89
LoveShooter <maxxx.aka.subfocus@gmail.com>
2020-04-20 23:34:25 +0300

    ok

--- mongodb_conn_input.py
+++ mongodb_conn_input.py
@@ -1,9 +1,33 @@
 import pymongo
 
+
+newdb = input("Enter DB name:")
+newcoll = input("Enter name of Collection:")
+
+
 client = pymongo.MongoClient('mongodb+srv://sysadm:Ff121314@cluster0-gpxwq.mongodb.net/')  #Connect to my MongoDB cluster + auth
 
-inputdb = input("Enter DB name: ")
-inputcol = input("Enter name of Collection: ")
 
-db = client['inputdb']  #Create DB
-mycol = db['inputcol']  #Create collection
+print(newdb)
+print(newcoll)
+
+#mydatabase = client.newdb
+#mycollection = mydatabase.newcoll
+
+db = client['newdb']  #Create DB
+mycol = db['newcoll']  #Create collection
+
+
+#print(mydatabase)
+#print(mycollection)
+
+#mycollection.insert_one({
+#    "_id": 1,
+#    "name": "pizza",
+#    "calories": 266,
+#    "fats": {
+#        "saturated": 4.5,
+#        "trans": 0.2
+#    },
+#    "protein": 11
+#})
