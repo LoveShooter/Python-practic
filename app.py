29ba0e3f43117b05f1f90f083a4aa572e8cb05f7
LoveShooter <maxxx.aka.subfocus@gmail.com>
2020-05-03 12:35:10 +0300

    ver1

--- app.py
+++ app.py
@@ -48,12 +48,32 @@ def add_data():
     return jsonify({'result': output})
 
 
-@app.route('/deldata/<int:user_id>', methods=['GET'])
+@app.route('/del_data/<int:user_id>', methods=['GET'])
 def del_one_data(user_id):
     usercoll = mongo.db.users
     usercoll.delete_one({'id': user_id}) # Delete data by user ID
 
     return jsonify('Task deleted successfully!')
+
+
+
+@app.route('/add_data_placeholder', methods=['GET'])  # Send a request to the API and add data in mongodb from jsonplaceholder
+def add_data_placeholder():
+    mycoll = mongo.db.users
+    
+    url1 = 'https://jsonplaceholder.typicode.com/users/6'
+    url2 = 'https://jsonplaceholder.typicode.com/users/5'
+    url3 = 'https://jsonplaceholder.typicode.com/users/8'
+    url4 = 'https://jsonplaceholder.typicode.com/users/10'
+    url5 = 'https://jsonplaceholder.typicode.com/users/4'
+    
+    urls = [url1, url2, url3, url4, url5]
+
+    url = requests.get(random.choice(urls))
+    userlist = json.loads(url.text)
+    mycoll.insert_one(userlist)
+    
+    return userlist
 
 
 if __name__ == '__main__':
