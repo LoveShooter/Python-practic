71100c53fb21bd0234b0ddeeae14598b61667462
LoveShooter <maxxx.aka.subfocus@gmail.com>
2020-05-05 03:06:44 +0300

    v5

--- app.py
+++ app.py
@@ -91,23 +91,23 @@ def del_one_data(id):
 #    return resp
 
 
-@app.route('/add_data_placeholder', methods=['GET'])  # Send a request to the API and add data in mongodb from jsonplaceholder
-def add_data_placeholder():
-    mycoll = mongo.db.users
-    
-    url1 = 'https://jsonplaceholder.typicode.com/users/6'
-    url2 = 'https://jsonplaceholder.typicode.com/users/5'
-    url3 = 'https://jsonplaceholder.typicode.com/users/8'
-    url4 = 'https://jsonplaceholder.typicode.com/users/10'
-    url5 = 'https://jsonplaceholder.typicode.com/users/4'
-    
-    urls = [url1, url2, url3, url4, url5]
-
-    url = requests.get(random.choice(urls))
-    userlist = json.loads(url.text)
-    mycoll.insert_one(userlist)
-    
-    return userlist
+#@app.route('/add_data_placeholder', methods=['GET'])  # Send a request to the API and add data in mongodb from jsonplaceholder
+#def add_data_placeholder():
+#    mycoll = mongo.db.users
+#    
+#    url1 = 'https://jsonplaceholder.typicode.com/users/6'
+#    url2 = 'https://jsonplaceholder.typicode.com/users/5'
+#    url3 = 'https://jsonplaceholder.typicode.com/users/8'
+#    url4 = 'https://jsonplaceholder.typicode.com/users/10'
+#    url5 = 'https://jsonplaceholder.typicode.com/users/4'
+#    
+#    urls = [url1, url2, url3, url4, url5]
+#
+#    url = requests.get(random.choice(urls))
+#    userlist = json.loads(url.text)
+#    mycoll.insert_one(userlist)
+#    
+#    return userlist
 
 
 @app.errorhandler(404)
