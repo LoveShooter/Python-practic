1321f22f2551999d32b6f6d0b67a3b51e0611391
LoveShooter <maxxx.aka.subfocus@gmail.com>
2020-05-14 01:04:44 +0300

    upd3

--- api-dwn-upl.py
+++ api-dwn-upl.py
@@ -49,5 +49,44 @@ def postFile(filename):
     return jsonify(201, 'File uploaded successfully!') # Return 201 CREATED
 
 
+@app.route('/delfiles/<filename>', methods=['DELETE'])
+def delFile(filename):
+    
+    files = []
+
+    for filename in os.listdir(UPLOAD_DIRECTORY):
+        path = os.path.join(UPLOAD_DIRECTORY, filename)
+        if os.path.isfile(path):
+            files.append(filename)
+            os.remove(path)
+            response = {"message": "File Deleted"}
+        else:
+            response = {"message": "File Not Found!"}
+    
+    return jsonify(response), 200
+
+#files = []
+
+#    for filename in os.listdir(UPLOAD_DIRECTORY):
+#        path = os.path.join(UPLOAD_DIRECTORY, filename)
+#        if os.path.exists(filename):
+#            files.append(filename)
+#    return jsonify(files)
+
+#if os.path.exists("demofile.txt"):
+#    os.remove("demofile.txt")
+
+@app.errorhandler(404)
+def not_found(error=None):
+    message = {
+        'status': 404,
+        'message': 'Not Found: ' + request.url,
+    }
+    resp = jsonify(message)
+    resp.status_code = 404
+
+    return resp
+
+
 if __name__ == "__main__":
     app.run(debug=True, port=8000)
