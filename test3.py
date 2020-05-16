a08d13a00f7a5b0ab0d69d7c6e54978dc670c475
LoveShooter <maxxx.aka.subfocus@gmail.com>
2020-05-16 22:01:43 +0300

    Create test3.py

---
+++ test3.py
@@ -0,0 +1,14 @@
+import os
+
+path = 'G:\OneDrive\coding\python\PythonPractice\file.txt'
+
+
+isFile = os.path.isfile(path)
+print(isFile)
+
+
+
+path = 'G:/OneDrive/coding/python'
+
+isFile = os.path.isfile(path)
+print(isFile)
