6e990988ccee272ad3f651fba5f3ca79c07a05ac
LoveShooter <maxxx.aka.subfocus@gmail.com>
2020-07-11 02:10:52 +0300

    array_2x

---
+++ test-os-module3.py
@@ -0,0 +1,20 @@
+import os
+
+path = 'G:/OneDrive/coding/python/PythonPractice/testfolder/api_uploaded_files'
+
+
+def createMultipleFolders():
+    os.chdir(path)
+    folders = ['2001','2002','2003', ['122', '123'], ['2004', '2005']] 
+    for folder in folders:
+        for elem in folder:
+            if not os.path.exists(folder):
+                os.mkdir(folder)
+                print("Folders", folder, "created successfully")
+            else:
+                print("Folders", folder, "already exists!")
+
+
+
+if __name__ == '__main__':
+    createMultipleFolders()
