abaac860898cd028d50745424ec0bd18769ba8a4
LoveShooter <maxxx.aka.subfocus@gmail.com>
2020-07-11 02:21:42 +0300

    upd_array_z2

--- test-os-module3.py
+++ test-os-module3.py
@@ -5,11 +5,11 @@ path = 'G:/OneDrive/coding/python/PythonPractice/testfolder/api_uploaded_files'
 
 def createMultipleFolders():
     os.chdir(path)
-    folders = ['2001','2002','2003', ['122', '123'], ['2004', '2005']] 
+    folders = [['2001','2002','2003'], ['122', '123'], ['2004', '2005']] 
     for folder in folders:
         for elem in folder:
-            if not os.path.exists(folder):
-                os.mkdir(folder)
+            if not os.path.exists(elem):
+                os.mkdir(elem)
                 print("Folders", folder, "created successfully")
             else:
                 print("Folders", folder, "already exists!")
