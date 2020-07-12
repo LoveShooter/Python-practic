b454563103b9417b5500ee51cb4adaa08d8439a5
LoveShooter <maxxx.aka.subfocus@gmail.com>
2020-07-13 01:42:32 +0300

    upd

--- test-os-module3.py
+++ test-os-module3.py
@@ -2,19 +2,34 @@ import os
 
 path = 'G:/OneDrive/coding/python/PythonPractice/testfolder/api_uploaded_files'
 
+from dir_names import * # Imports the list of folders from dir_names.py file
 
-def createMultipleFolders():
+
+main_dir = [gryffindor, slytherin, hufflepuff, ravenclaw]			# Loading the list of sub-directories
+root_dir = 'Hogwarts Houses'
+main_dir_names = ['Gryffindor', 'Slytherin', 'Hufflepuff','Ravenclaw'] # Name of the sub-directories
+def main():
+    # Create directory
     os.chdir(path)
-    folders = ['1', ['2001','2002','2003'], ['122', '123'], ['2004', '2005']] 
-    for folder in folders:
-        for elem in folder:
-            if not os.path.exists(elem):
-                os.mkdir(elem)
-                print("Folders", folder, "created successfully")
-            else:
-                print("Folders", folder, "already exists!")
+    for i in range(0, len(main_dir)):
+        for j in range(0,len(main_dir[i])):
+            dirName = str(root_dir) + '/' + str(main_dir_names[i]) +'/' + str(main_dir[i][j])
+            
+            
+            try:
+                # Create target Directory
+                os.makedirs(dirName)
+                print("Directory " , dirName ,  " Created ") 
+            except FileExistsError:
+                print("Directory " , dirName ,  " already exists")        
 
+            # Create target Directory if don't exist
+            if not os.path.exists(dirName):
+                os.makedirs(dirName)
+                print("Directory " , dirName ,  " Created ")
+            else:    
+                print("Directory " , dirName ,  " already exists")
 
 
 if __name__ == '__main__':
-    createMultipleFolders()
+    main()
