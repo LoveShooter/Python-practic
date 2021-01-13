7011b2e15f24a620b3669e9a56e1dec2e1b2f42e
LoveShooter <maxxx.aka.subfocus@gmail.com>
2021-01-13 17:23:22 +0300

    upd script

--- check-site.py
+++ check-site.py
@@ -4,7 +4,7 @@ page = requests.get(url)
 urlStatus = page.status_code 
 #print(page)
 #print(urlStatus)
-if ( urlStatus != 200 ):
+if ( urlStatus > 200 != 200 ):
     print("HTTP Status:",urlStatus, "BAD")
 else:
     print ("HTTP Status:",urlStatus, "GOOD") 
