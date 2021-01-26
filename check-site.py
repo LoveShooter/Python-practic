4814bc3019a956a753acd2118232c9ab4bdba87c
LoveShooter <maxxx.aka.subfocus@gmail.com>
2021-01-26 15:54:10 +0300

    upd restart service

--- check-site.py
+++ check-site.py
@@ -9,3 +9,10 @@ if ( urlStatus > 200 != 200 ):
     print("HTTP Status:",urlStatus, "BAD")
 else:
     print ("HTTP Status:",urlStatus, "GOOD") 
+
+
+    
+try:
+    page1 = requests.get(url)
+except requests.exceptions.ConnectionError:
+    r.status_code = "Connection refused"
--- restartservice.py
+++ restartservice.py
@@ -1,19 +1,21 @@
 import requests
 import subprocess
 
+from urllib3 import disable_warnings, exceptions
+disable_warnings(exceptions.InsecureRequestWarning)
+
 # ENV
 url = "https://yandex.ru"
-page = requests.get(url)
-urlStatus = page.status_code 
-
+page = requests.get(url, verify=False)
+urlStatus = page.status_code
 
 #print(page)
 #print(urlStatus)
 
 # Checking code of HTTP response
 # If BAD status then Restart Service
-if ( urlStatus > 200 != 200 ):
-    serviceRestart = subprocess.run(['powershell', '-Command','Restart-Service -Name spooler'])
+if ( urlStatus > 200, urlStatus != 200 ):
+    subprocess.run(['powershell', '-Command','Restart-Service -Name PowerBIReportServer'])
     print("HTTP Status:",urlStatus, "BAD. Service restarted!")
 else:
     print ("HTTP Status:",urlStatus, "GOOD. No need Restart.") 
